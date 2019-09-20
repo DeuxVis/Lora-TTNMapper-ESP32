4942ba665e349e0f70bda02c8c169455b20a8336
Alvaro Figueroa <alvaro@greencore.co.cr>
2019-09-20 14:31:22 -0600

    Agregando parcial de ap+wifi, para encodear correctamente

--- Lora-TTNMapper-T-Beam.ino
+++ Lora-TTNMapper-T-Beam.ino
@@ -1,6 +1,12 @@
 #include <lmic.h>
 #include <hal/hal.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
+const char *ssid = "chispa";
+
+WiFiServer server(80);
 
 // UPDATE the config.h file in the same folder WITH YOUR TTN KEYS AND ADDR.
 #include "config.h"
@@ -133,8 +139,11 @@ void setup() {
   Serial.begin(115200);
   Serial.println(F("TTN Mapper"));
   
-  //Turn off WiFi and Bluetooth
-  WiFi.mode(WIFI_OFF);
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+  server.begin();
+  
+//  WiFi.mode(WIFI_OFF);
   btStop();
   gps.init();
 
@@ -186,4 +195,71 @@ void setup() {
 
 void loop() {
     os_runloop_once();
+      WiFiClient client = server.available();   // listen for incoming clients
+ if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("<html>");
+            client.print("<title>Foo</title>");
+            client.print("<head> <meta http-equiv=\"refresh\" content=\"30\"> </head>");
+            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
+            client.print(txBuffer[0]);client.print(txBuffer[1]);client.print(txBuffer[2]);client.print(txBuffer[3]);client.print(txBuffer[4]);client.print(txBuffer[5]);client.print(txBuffer[6]);client.print(txBuffer[7]);client.print(txBuffer[8]);client.print(txBuffer[9]);
+
+            Lat = ( txBuffer[0] >> 16 ) & 0xFF;
+            Lat = ( txBuffer[1] >> 8 ) & 0xFF;
+            Lat = txBuffer[2] & 0xFF;
+/*
+  txBuffer[3] = ( LongitudeBinary >> 16 ) & 0xFF;
+  txBuffer[4] = ( LongitudeBinary >> 8 ) & 0xFF;
+  txBuffer[5] = LongitudeBinary & 0xFF;
+
+  altitudeGps = tGps.altitude.meters();
+  txBuffer[6] = ( altitudeGps >> 8 ) & 0xFF;
+  txBuffer[7] = altitudeGps & 0xFF;
+
+  hdopGps = tGps.hdop.value()/10;
+  txBuffer[8] = hdopGps & 0xFF;
+  */
+            client.print("</html>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+  }
 }
